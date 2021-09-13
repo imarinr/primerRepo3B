89de2f5318baf8ed186818ed86f4ba1faa415da8
Ivan Marín <imarinr@outlook.com>
2021-09-13 07:28:25 -0500

    Create operaciones_basicas.py

---
+++ operaciones_basicas.py
@@ -0,0 +1,12 @@
+# En este script hacemos operaciones basicas
+# 13 de sept 2021
+suma = 5 + 2
+resta = suma - 8.5
+m1 = 32
+m2 = 64
+multiplicacion = m1 * m2
+print("Este programa acaba de hacer algunas operaciones")
+print('la suma dio como resultado: ')
+print(suma)
+print('la resta dio como resultado: ' + resta) # la resta dio como resultado: -1.5
+print('la multiplicacion dio como resultado: ' + multiplicacion)
