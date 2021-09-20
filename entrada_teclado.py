f91bee690896a2003d05e9c8be45ca86c38a0401
Ivan Marín <imarinr@outlook.com>
2021-09-20 12:46:38 +0000

    Hice el ejercicio que se quedo pendiente de la clase pasada

---
+++ entrada_teclado.py
@@ -0,0 +1,13 @@
+print('Como te llamas?')
+nombre = input()
+print('Hola ', nombre, 'Espero que estes muy bien')
+
+print('Hoy es 15 de septiembre, vas a festejar?')
+respuesta = input()
+
+if respuesta == 'si':
+  print('Genial! diviertete mucho') # se pone identacion para que el interprete reconozca que esta instruccion va dentro de if
+else:
+  print('pues f por ti')
+
+
---
+++ suma_resta.py
@@ -0,0 +1,25 @@
+
+# 1. Inicio
+# 2. Definir resultado, entrada, num1, num2
+# 3. Imprimir "Que operacion quieres hacer?"
+print("Que operacion quieres hacer?")
+# 4. entrada = entrada de usuario
+entrada = input()
+# 5. Imprimir "dame el primer numero"
+print("dame el primer numero")
+# 6. num1 = entrada de usuario
+num1 = float(input())
+# 7. Imprimir "dame el segundo numero"
+print("dame el segundo numero")
+# 8. num2 = entrada de usuario
+num2 = float(input())
+# 9. Si entrada = "suma" entonces,
+resultado = 0
+if entrada == "suma":
+  resultado = num1 + num2 # 9.1. resultado = num1 + num2,
+else:
+  resultado = num1 - num2# 9.2. de lo contrario, resultado = num1 - num2
+# 10. Imprimir "el resultado de la " entrada "es " resultado
+print("El resultado de la ", entrada, " es ", resultado)
+# 11. Fin
+
